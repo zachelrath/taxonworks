9e7a77d6874753efe7c3fc67b389aecaae5bb237
José Luis Pereira <pepelui@outlook.com>
2020-02-19 19:57:06 -0300

    Created colletors component

---
+++ app/javascript/vue/tasks/collection_objects/filter/components/filters/collectors.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Collectors</h2>
+    <div>
+      <label>Verbatim collectors</label>
+      <input
+        v-model="collectors.verbatim_collectors"
+        type="text">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      collectors: {
+        verbatim_collectors
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
